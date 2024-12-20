ea7e87d4b2e3c9370eb1931eb85d1c2de39d0b13
unknown <prasadsingatkar360@gmail.com>
2024-12-20 16:49:48 +0530

    HibernateJPA CRUD

---
+++ cruddemo/src/test/java/com/flynaut/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flynaut.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
